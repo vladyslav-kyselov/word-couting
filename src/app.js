6a724120854191a72abefce06dd75572fee88708
vladikyss <vladikyss7777@mail.ru>
2018-10-10 13:48:04 +0300

    Update src/app.js

--- src/app.js
+++ src/app.js
@@ -8,24 +8,24 @@ function onCalculcateWordsButtonClick() {
   let line = document.getElementById('input').value;
   line = line.replace(/[^A-Za-zА-Яа-яЁё\s]/g, '').replace(/\s+/gi, ' ');
   let NumberOfWords = 0;
-  for (let i = 0; i <= line.length; i++) {
-    let test = line.charAt(line.length-i);
+  for (let i = 0; i < line.length; i++) {
+    let test = line[i];
     if (test == ' ') {
       NumberOfWords++;
     }
   }
-  let firstNoSpace = line.charAt(line.length[0]);
-  let LineLastWord = line.charAt(line.length - 1);
+  let firstNoSpace = line[0];
+  let LineLastWord = line[line.length - 1];
   if ( LineLastWord == ' ') {
     NumberOfWords--;
   }
-  if (line.length[0] == ' ') {
-    NumberOfWords--;
-  }
+
   if (line.length == 0) {
     NumberOfWords= 0;
   } else if (firstNoSpace != ' ') {
     NumberOfWords++;
+  } else if ( line.length[0] == ' ') {
+    NumberOfWords--;
   }
 
   document.getElementById('result').innerHTML = 'В этой строке ' + NumberOfWords + ' слова';
