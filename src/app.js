d4740dc34220b5f0c2f39cbabb0b94a32de364f9
vladikyss <vladikyss7777@mail.ru>
2018-10-10 16:12:05 +0300

    add new function

--- src/app.js
+++ src/app.js
@@ -4,29 +4,23 @@ function buttonClick() {
   document.getElementById('button-click').addEventListener('click', onCalculcateWordsButtonClick);
 }
 
+function getWords(str) {
+  let NumberOfWords = 0;
+  for (let i = 0; i < str.length; i++) {
+    let check = str[i];
+    if (check == ' ') NumberOfWords++;
+  }
+  let LineLastWord = str[str.length - 1];
+  if ( LineLastWord == ' ') NumberOfWords--;
+
+  if (str.length == 0) NumberOfWords= 0;
+  else if (str[0] != ' ') NumberOfWords++;
+  return NumberOfWords;
+}
+
 function onCalculcateWordsButtonClick() {
   let line = document.getElementById('input').value;
   line = line.replace(/[^A-Za-zА-Яа-яЁё\s]/g, '').replace(/\s+/gi, ' ');
-  let NumberOfWords = 0;
-  for (let i = 0; i < line.length; i++) {
-    let test = line[i];
-    if (test == ' ') {
-      NumberOfWords++;
-    }
-  }
-  let firstNoSpace = line[0];
-  let LineLastWord = line[line.length - 1];
-  if ( LineLastWord == ' ') {
-    NumberOfWords--;
-  }
-
-  if (line.length == 0) {
-    NumberOfWords= 0;
-  } else if (firstNoSpace != ' ') {
-    NumberOfWords++;
-  } else if ( line.length[0] == ' ') {
-    NumberOfWords--;
-  }
-
-  document.getElementById('result').innerHTML = 'В этой строке ' + NumberOfWords + ' слова';
+  let words = getWords(line);
+  document.getElementById('result').innerHTML = 'В этой строке ' + words + ' слова';
 }
